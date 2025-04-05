c3d428cbfd198679a2436aa3127373f2df58a143
Jm's Laptop <ljm12543@gmail.com>
2025-04-05 10:35:06 -0400

    revalidation added

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -1,3 +1,4 @@
+import { NextRequest, NextResponse } from "next/server";
 import {
 	HIDDEN_PRODUCT_TAG,
 	SHOPIFY_GRAPHQL_API_ENDPOINT,
@@ -42,6 +43,8 @@ import {
 	ShopifyRemoveFromCartOperation,
 	ShopifyUpdateCartOperation,
 } from "./types";
+import { headers } from "next/headers";
+import { revalidateTag } from "next/cache";
 
 const rawDomain = process.env.SHOPIFY_STORE_DOMAIN || "";
 const domain = ensureStartWith(
@@ -406,6 +409,47 @@ export async function getCart(
   }
 
   return reshapeCart(res.body.data.cart);
+}
+
+// This is called from `app/api/revalidate.ts` so providers can control revalidation logic.
+export async function revalidate(req: NextRequest): Promise<NextResponse> {
+  // We always need to respond with a 200 status code to Shopify,
+  // otherwise it will continue to retry the request.
+
+  const collectionWebhooks = [
+    "collections/create",
+    "collections/delete",
+    "collections/update",
+  ];
+  const productWebhooks = [
+    "products/create",
+    "products/delete",
+    "products/update",
+  ];
+  const topic = (await headers()).get("x-shopify-topic") || "unknown";
+  const secret = req.nextUrl.searchParams.get("secret");
+  const isCollectionUpdate = collectionWebhooks.includes(topic);
+  const isProductUpdate = productWebhooks.includes(topic);
+
+  if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
+    console.error("Invalid revalidation secret.");
+    return NextResponse.json({ status: 200 });
+  }
+
+  if (!isCollectionUpdate && !isProductUpdate) {
+    // We don't need to revalidate anything for any other topics.
+    return NextResponse.json({ status: 200 });
+  }
+
+  if (isCollectionUpdate) {
+    revalidateTag(TAGS.collections);
+  }
+
+  if (isProductUpdate) {
+    revalidateTag(TAGS.products);
+  }
+
+  return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
 }
 
 // export async function getPage(handle: string): Promise<Page> {
