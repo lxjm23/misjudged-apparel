b1151580c45768b84fb0674abdb0422468851fc6
Jm's Laptop <ljm12543@gmail.com>
2025-04-06 15:02:15 -0400

    openimage fixed

--- src/components/opengraph-image.tsx
+++ src/components/opengraph-image.tsx
@@ -27,16 +27,7 @@ export default async function OpengraphImage(
     {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: "Inter",
-          data: await fetch(
-            new URL("../fonts/Inter-Bold.ttf", import.meta.url)
-          ).then((res) => res.arrayBuffer()),
-          style: "normal",
-          weight: 700,
-        },
-      ],
+      
     }
   );
 }
